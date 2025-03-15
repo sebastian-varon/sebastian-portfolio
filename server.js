76eb3e7771c8c9656f621bed0cc2a4645b87c941
sebastian-varon <sebasvaronmoreno@hotmail.com>
2025-03-15 00:07:59 -0400

    Changed server to 5050

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 
 // Middleware
 app.use(cors());
